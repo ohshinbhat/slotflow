1ec612680a0cfb35d6764571165c5075d5d4c229
zokhcat <understandingzak@gmail.com>
2024-09-18 01:34:53 +0530

    chore(user): fetch and update user utilities

---
+++ src/utils/getUser.js
@@ -0,0 +1,18 @@
+import { supabase } from "../config/supabase";
+
+const getUser = async (email) => {
+  const { data, error } = supabase
+    .from("users")
+    .select("full_name, username, email, profile_desc, profile_pic, created_at")
+    .eq("email", email)
+    .single();
+
+  if (error) {
+    console.error("Error fetching user data:", error);
+    return null;
+  }
+
+  return data;
+};
+
+export default getUser;
